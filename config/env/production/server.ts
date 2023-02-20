34d15f56a94fe70a617c5372a14d1549935c4e6e
sydneehaley <hello@sydneehaley.com>
2023-02-19 20:52:02 -0500

    database config update

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -1,3 +1,8 @@
 export default ({ env }: { env: any }) => ({
   url: env("MY_HEROKU_URL"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+    },
+  },
 });
