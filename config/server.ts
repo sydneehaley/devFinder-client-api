2af02f96da14593d8a55eeaf7fad1890fac8e146
sydneehaley <hello@sydneehaley.com>
2023-02-19 20:56:55 -0500

    database config update

--- config/server.ts
+++ config/server.ts
@@ -1,10 +1,15 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
+    },
   },
 });
