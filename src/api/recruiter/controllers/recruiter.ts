540d1fc8d1dd45eaa969cf806532d626e20e9162
sydneehaley <hello@sydneehaley.com>
2023-02-20 23:39:44 -0500

    api content update

---
+++ src/api/recruiter/controllers/recruiter.ts
@@ -0,0 +1,7 @@
+/**
+ * recruiter controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::recruiter.recruiter');
---
+++ src/api/recruiter/routes/recruiter.ts
@@ -0,0 +1,7 @@
+/**
+ * recruiter router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::recruiter.recruiter');
